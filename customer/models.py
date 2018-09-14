921dd3789f7f9692610df1133d36640823599813
Memorydemoeric <1622902230@qq.com>
2018-09-14 16:38:53 +0800

    添加 moels 字段

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+# 客户信息
+class CustomerInfo(models.Model):
+    cust_name = models.CharField(max_length=64, null=False)
+    cust_local = models.CharField(max_length=256, null=False)
+    cust_mobile_phone = models.CharField(max_length=32, default='-')
+    cust_phone_number = models.CharField(max_length=32, default='-')
+    cust_rebate = models.FloatField(default=100.00)
+    cust_address = models.CharField(max_length=1024)
+    is_delete = models.BooleanField(default=0)
+
+    class Meta:
+        db_table = 'syc_cust_info'
--- purchase/models.py
+++ purchase/models.py
@@ -1,3 +1,36 @@
+import datetime
+
 from django.db import models
 
 # Create your models here.
+
+# 订单
+class Purchase(models.Model):
+    pur_location_name = models.CharField(max_length=64)
+    pur_cust_name = models.CharField(max_length=64)
+    pur_price = models.FloatField(default=0.00)
+    pur_create_date = models.DateTimeField(auto_now_add=True)
+    pur_modify_date = models.DateField(default= datetime.datetime.now().strftime('%Y-%m-%d'))
+    pur_finished_date = models.DateField(auto_now=True)
+    pur_handle = models.CharField(max_length=256)
+    cust_id = models.ForeignKey('CustomerInfo', on_delete=models.PROTECT)
+    is_finished = models.BooleanField(default=0)
+    is_delete = models.BooleanField(default=0)
+
+    class Meta:
+        db_table = 'syc_purchase'
+
+
+# 订单详情
+class PurchaseDetail(models.Model):
+    pur_pro_id = models.CharField(max_length=64, null=False)
+    pur_pro_type = models.CharField(max_length=256, null=False)
+    pur_pro_count = models.IntegerField(default=0)
+    pur_finished_pro_count = models.IntegerField(default=0)
+    pur_pro_unit_price = models.FloatField(default=0.00)
+    pur_pro_price = models.FloatField(default=0.00)
+    pur_id = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    is_delete = models.BooleanField(default=0)
+
+    class Meta:
+        db_table = 'syc_pur_detail'
